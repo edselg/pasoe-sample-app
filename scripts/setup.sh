251b1a0c0d6e117ba02c2fec5fdf814ac3c0b414
edselg <edselg@rocketmail.com>
2021-09-20 09:52:16 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-export TERM=vt100
+export DLC=/psc/dlc
+export PATH=$DLC/bin:$PATH
 docker login
 sudo yum -y install htop 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 if [ ! -d /usr/lib/jvm/jdk ]
 then
@@ -10,8 +13,11 @@ then
 fi
 
 mkdir -p ~/install
-aws s3 cp s3://mysupportfiles2/12.3.0.tar.gz ~/install
-tar xzvCf ~/install ~/install/12.3.0.tar.gz
+if [ ! -f /home/ec2-user/install/12.3.0.tar.gz ]
+then
+    aws s3 cp s3://mysupportfiles2/12.3.0.tar.gz ~/install
+    tar xzvCf ~/install ~/install/12.3.0.tar.gz
+fi
 
 if [ ! -d /psc ]
 then
@@ -21,8 +27,16 @@ fi
 cp -f /psc/dlc/progress.cfg ~/pasoe-sample-app/oedb/build/license
 cp -f /psc/dlc/progress.cfg ~/pasoe-sample-app/deploy/license
 
-cp ~/pasoe-sample-app/oedb/build
+cd ~/pasoe-sample-app/oedb/build
 ./build.sh
 
 docker run -d -p 20000:20000 -p 3000-3200:3000-3200 -e DB_BROKER_PORT=20000 -e DB_MINPORT=3000 -e DB_MAXPORT=3200 oedb1
+
+cd ~/pasoe-sample-app/Sports
+proant package
+cp ~/pasoe-sample-app/Sports/output/package-output/Sports.zip ~/pasoe-sample-app/deploy/ablapps/
+
+cd ~/pasoe-sample-app/deploy
+proant deploy
+
 #
