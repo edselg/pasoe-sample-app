1386568f87f65595a5d881b8c8362946af4c7438
EC2 Default User <ec2-user@ip-172-31-67-246.ec2.internal>
2021-10-17 13:52:22 +0000

    Update

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 export PROJECT_HOME=~/environment/pasoe-sample-app
 export DLC=/psc/dlc
 export PATH=$DLC/bin:$PATH
+export DOCKER_BUILDKIT=1
 
 export PRIVATE_IP_ADDRESS=`aws ec2 describe-instances | jq -r '.Reservations[].Instances[] | select(.State.Name == "running" and .Tags[].Value == "dev1") | .PrivateIpAddress'`
 
@@ -14,3 +15,6 @@ sed -i "s/PRIVATE_IP_ADDRESS/${PRIVATE_IP_ADDRESS}/" $PROJECT_HOME/Sports/conf/s
 cd $PROJECT_HOME/Sports
 proant package
 cp $PROJECT_HOME/Sports/output/package-output/Sports.zip $PROJECT_HOME/deploy/ablapps/
+
+cd $PROJECT_HOME/deploy
+proant package
